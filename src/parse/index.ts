f7eea0165d642168207387458a65897d7807fafe
Douglas Guimarães <eu@rgdoug.com>
2022-05-15 23:19:43 -0300

    ws fix

--- src/parse/index.ts
+++ src/parse/index.ts
@@ -2,6 +2,6 @@ import { instructions } from "../native/instructions";
 
 // TODO: handle literals and identificators
 export const matchSymbols = (literal: string): any =>
-instructions[literal] !== undefined ? instructions[literal] : literal;
+  instructions[literal] !== undefined ? instructions[literal] : literal;
 
 export const parse = (line: string) => line.split(" ").map(matchSymbols);
