7e8f543b8779cb3809de0c51c7847eb0464894d3
Douglas Guimarães <eu@rgdoug.com>
2022-05-16 15:25:25 -0300

    remove unused shebang

--- repl/mod.ts
+++ repl/mod.ts
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 import { parse } from "../src/parser.ts";
 import { VM } from "../src/vm/mod.ts";
 
@@ -8,12 +6,12 @@ const vm: VM = new VM();
 export async function interactionLoop(shouldAsk: boolean): Promise<void> {
   if (shouldAsk) {
     const buf = new Uint8Array(1024);
-await Deno.stdout.write(new TextEncoder().encode("INSTRUCTION> "));
-    const n = <number>await Deno.stdin.read(buf);
+    await Deno.stdout.write(new TextEncoder().encode("INSTRUCTION> "));
+    const n = <number> await Deno.stdin.read(buf);
     const line = new TextDecoder().decode(buf.subarray(0, n)).trim();
     if (line === "EXIT") interactionLoop(false);
     else {
-      vm.process(parse(line as string));
+      vm.process(parse(line));
       interactionLoop(true);
     }
   } else Deno.exit();
