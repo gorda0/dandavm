62b517dbec050406431851e8107c80d2fef14191
Douglas Guimarães <eu@rgdoug.com>
2022-05-16 00:18:55 -0300

    matching args

--- src/native/logical.ts
+++ src/native/logical.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { LogicalInstruction } from "./instructions";
-// TODO: args
-export const and: LogicalInstruction = ({ args }) => args[0] && args[1];
-export const or: LogicalInstruction = ({ args }) => args[0] || args[1];
-export const not: LogicalInstruction = ({ args }) => !args[0];
-export const lsst: LogicalInstruction = ({ args }) => args[0] < args[1];
-export const grtr: LogicalInstruction = ({ args }) => args[0] > args[1];
-export const equal: LogicalInstruction = ({ args }) => args[0] === args[1];
-export const diff: LogicalInstruction = ({ args }) => args[0] !== args[1];
-export const greq: LogicalInstruction = ({ args }) => args[0] >= args[1];
-export const lseq: LogicalInstruction = ({ args }) => args[0] <= args[1];
+
+export const and: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 && arg1;
+export const or: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 || arg1;
+export const not: LogicalInstruction = ({ args: [arg0] }) => !arg0;
+export const lsst: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 < arg1;
+export const grtr: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 > arg1;
+export const equal: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 === arg1;
+export const diff: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 !== arg1;
+export const greq: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 >= arg1;
+export const lseq: LogicalInstruction = ({ args: [arg0, arg1] }) => arg0 <= arg1;
--- src/native/operators.ts
+++ src/native/operators.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { OperatorInstruction } from "./instructions";
-// TODO: args
-export const add: OperatorInstruction = ({ args }) => args[0] + args[1];
-export const subtract: OperatorInstruction = ({ args }) => args[0] - args[1];
-export const multiply: OperatorInstruction = ({ args }) => args[0] * args[1];
-export const divide: OperatorInstruction = ({ args }) => args[0] / args[1];
-export const mod: OperatorInstruction = ({ args }) => args[0] % args[1];
+
+export const add: OperatorInstruction = ({ args: [arg0, arg1] }) => arg0 + arg1;
+export const subtract: OperatorInstruction = ({ args: [arg0, arg1] }) => arg0 - arg1;
+export const multiply: OperatorInstruction = ({ args: [arg0, arg1] }) => arg0 * arg1;
+export const divide: OperatorInstruction = ({ args: [arg0, arg1] }) => arg0 / arg1;
+export const mod: OperatorInstruction = ({ args: [arg0, arg1] }) => arg0 % arg1;
