e3d2830a1f6c167e57d423ee542a76e8fa219dde
Douglas Guimarães <eu@rgdoug.com>
2022-05-17 00:11:40 -0300

    bin fixes

--- scripts/build_cli.ts
+++ scripts/build_cli.ts
@@ -2,14 +2,15 @@ import { build, emptyDir } from "https://deno.land/x/dnt@0.23.0/mod.ts";
 import { packageDefaults } from "./helpers.ts";
 
 const CLI_DIR = "./npm_cli";
-const ESM_PATH = "/esm/cli.js";
-const FULL_PATH = CLI_DIR + ESM_PATH;
-const shebang = "#!/usr/bin/env node \n";
 
 await emptyDir(CLI_DIR);
 
 await build({
-  entryPoints: ["./cli.ts"],
+  entryPoints: [{
+    kind: "bin",
+    name: "danda",
+    path: "./cli.ts",
+  }],
   outDir: CLI_DIR,
   shims: {
     deno: true,
@@ -19,14 +20,5 @@ await build({
     name: "@dandavm/cli",
     description: "dandavm cli",
     ...packageDefaults,
-    bin: {
-      "danda": "." + ESM_PATH,
-    },
-  },
+  }
 });
-
-const cliContent = await Deno.readTextFile(FULL_PATH);
-
-await Deno.writeTextFile(FULL_PATH, shebang + cliContent);
-
-await Deno.chmod(FULL_PATH, 0o764);
