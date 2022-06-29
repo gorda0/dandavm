3a8396ed394563cd6901fe7b4760bf369692581d
Douglas Guimarães <eu@rgdoug.com>
2022-06-29 19:04:52 -0300

    enhancing simple line benchmark

--- benchmark/simple_line.bench.ts
+++ benchmark/simple_line.bench.ts
@@ -1,9 +1,16 @@
 import { GenericToken } from "../src/native/instructions.ts";
-import { parse } from "../src/parser.ts";
+import { parse, printFile } from "../src/parser.ts";
 import { VM } from "../src/vm/mod.ts";
+import { existsSync } from "https://deno.land/std/fs/mod.ts";
+import { defer } from "../src/utils/fn.ts";
+import { unspace } from "../src/utils/string.ts";
 
-const { bench } = Deno;
+const BENCHMARK_TEMP_DIR = "./bench_tmp";
 const SIMPLE_LINE = "context Sum in end context Sub in end";
+
+const { bench, remove, mkdir } = Deno;
+
+const vm = new VM();
 
 const preprocessed = {
   simple: {
@@ -11,11 +18,28 @@ const preprocessed = {
   },
 };
 
-
-const vm = new VM();
+if (existsSync(BENCHMARK_TEMP_DIR)) {
+  remove(BENCHMARK_TEMP_DIR, { recursive: true });
+} else {
+  mkdir(BENCHMARK_TEMP_DIR);
+}
 
 bench("parse a simple line", { group: "benching" }, () => {
   parse(SIMPLE_LINE);
+});
+
+bench("parse and compile a simple line", { group: "benching" }, () => {
+  printFile(
+    `${BENCHMARK_TEMP_DIR}/${unspace("parse and compile a simple line")}`,
+    parse(SIMPLE_LINE),
+  );
+});
+
+bench("compile a preprocessed list of tokens", { group: "benching" }, () => {
+  printFile(
+    `${BENCHMARK_TEMP_DIR}/${unspace("compile a preprocessed list of tokens")}`,
+    preprocessed.simple.tokens,
+  );
 });
 
 bench(
@@ -28,8 +52,12 @@ bench(
 
 bench(
   "parse and process a simple line",
-  { group: "benching"},
+  { group: "benching" },
   () => {
     vm.process(parse(SIMPLE_LINE) as Array<GenericToken>);
   },
 );
+
+defer(() => {
+  remove(BENCHMARK_TEMP_DIR, { recursive: true });
+});
