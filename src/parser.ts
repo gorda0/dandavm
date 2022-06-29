1f97f12d05d0f69112b91cc17288101cda3c673f
Douglas Guimarães <eu@rgdoug.com>
2022-06-29 19:02:21 -0300

    add print method

--- src/parser.ts
+++ src/parser.ts
@@ -1,14 +1,17 @@
 import { GenericToken, instructions } from "./native/instructions.ts";
 
 function swapReverser(array: Array<GenericToken | string>) {
+  const length = array.length;
   let left = null;
   let right = null;
-  const length = array.length;
+
   for (left = 0, right = length - 1; left < right; left += 1, right -= 1) {
     const temporary = array[left];
+
     array[left] = array[right];
     array[right] = temporary;
   }
+
   return array;
 }
 
@@ -17,4 +20,7 @@ export const matchSymbols = (literal: string): GenericToken | string =>
   instructions[literal] || literal;
 
 export const parse = (line: string): Array<GenericToken | string> =>
-swapReverser(line.split(" ").map(matchSymbols));
+  swapReverser(line.split(" ").map(matchSymbols));
+
+export const printFile = (path: string, tokens: Array<GenericToken | string>) =>
+  Deno.writeTextFileSync(path, JSON.stringify({ tokens }));
